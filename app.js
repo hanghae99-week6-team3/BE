a24f79848ae213ed6fc992f172732b9ad0882e92
JiyuJang <apryll7995@gmail.com>
2022-08-15 10:55:26 +0900

    product, like 라우터 등록

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const indexRouter = require("./routes/");
+const productRouter = require("./routes/product");
+const likeRouter = require("./routes/like");
 
 require("dotenv").config();
 
@@ -9,7 +11,9 @@ const port = process.env.Port;
 
 app.use(express.json());
 
-app.use("/", [indexRouter]);
+app.use("/api", [indexRouter]);
+app.use("/api/product", [likeRouter]);
+app.use("/api/product", [productRouter]);
 
 app.listen(port, () => {
   console.log(port, "포트로 서버가 열렸어요!");
