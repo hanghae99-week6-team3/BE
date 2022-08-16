a5d46411b528092541ea3380f54aa94b63a5f32d
김대석 <skywind4225@gmail.com>
2022-08-16 11:20:10 +0900

    middlewares 병합 과정 커밋

--- middlewares/auth_middlewares.js
+++ middlewares/auth_middlewares.js
@@ -1,16 +1,9 @@
 const jwt = require("jsonwebtoken");
 const { User } = require("../models");
 
-<<<<<<< HEAD
 module.exports = (req, res, next) => {
   const tokenValue = req.cookies.token;
   if (!tokenValue) {
-=======
-
-module.exports = (req, res, next) => {
-const tokenValue = req.cookies.token;
-if (!tokenValue) {
->>>>>>> 작업중
     res
       .status(401)
       .json({ success: false, errorMessage: "로그인이 필요합니다." });
@@ -19,15 +12,9 @@ if (!tokenValue) {
 
   try {
     const { userId } = jwt.verify(tokenValue, "my-secret-key"); // userId 는 jwt.sign(userId : user._id)의 user._id가 할당된다.
-<<<<<<< HEAD
     User.findByPk(userId).then((user) => {
       res.locals.user = user;
       next();
-=======
-    
-    User.findByPk(userId).then((user) => {
-    next();
->>>>>>> 작업중
     });
   } catch (error) {
     res
@@ -35,8 +22,4 @@ if (!tokenValue) {
       .json({ success: false, errorMessage: "로그인이 필요합니다." });
     return;
   }
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 작업중
