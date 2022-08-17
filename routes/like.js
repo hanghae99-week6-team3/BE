806de208b192af50e08ddc7fd3775f003b2dad69
김대석 <skywind4225@gmail.com>
2022-08-17 17:04:44 +0900

    오류 수정(인증 미들웨어, package cors 누락, comment.controllers)

--- routes/like.js
+++ routes/like.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const authMiddleware = require("../middlewares/auth_middlewares");
 const LikeController = require("../controllers/like.controllers");
 const likeController = new LikeController();
 
