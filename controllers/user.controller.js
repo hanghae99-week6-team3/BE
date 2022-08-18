d8b741f3d7c32427923d6776d5ecf96ade362d71
Jiyu Jang <apryll7995@gmail.com>
2022-08-18 16:39:47 +0900

    로그인 실패 시 에러메시지 추가

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,50 +5,51 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 class UsersController {
-    userService = new UserService();
+  userService = new UserService();
 
-    //회원가입 ok
-    createUser = async(req,res,next) => {
-        const {userId, nickname, password} = req.body;
+  //회원가입 ok
+  createUser = async (req, res, next) => {
+    const { userId, nickname, password } = req.body;
 
-        await this.userService.createUser(userId, nickname, password);
-        res.status(200).json({
-            success: true
-        });
-    };
-    //login ok
-    Login = async(req,res,next) => {
-        const {userId, password} = req.body;
-        const user = await this.userService.Login(userId, password);
+    await this.userService.createUser(userId, nickname, password);
+    res.status(200).json({
+      success: true,
+    });
+  };
+  //login ok
+  Login = async (req, res, next) => {
+    const { userId, password } = req.body;
+    const user = await this.userService.Login(userId, password);
 
-        if(!user){
-            res.status(400).json({
-                success: false,
-            });
-            return;
-        };
-
-        let payload = {userId: user.userId, nickname: user.nickname};
-        const token = jwt.sign(payload, process.env.MYSQL_KEY, { expiresIn: "7d" });
-
-        res.json({
-            token,
-        });
-    };
-    //중복확인  ok
-    DoubleCheck = async(req,res,next) => {
-        const {key, value} = req.body;
-        let user;
-        if(key == "userId"){
-            user = await this.userService.findOneUser_userId(value);
-        }
-        if(key == "nickname"){
-            user = await this.userService.findOneUser_nickname(value);
-        }
-        if(!user){
-            return res.status(200).json({ok: true});
-        }
-       return res.status(200).json({ok: false});
+    if (!user) {
+      res.status(400).json({
+        success: false,
+        errorMessage: "ID or Password is wrong",
+      });
+      return;
     }
-};
+
+    let payload = { userId: user.userId, nickname: user.nickname };
+    const token = jwt.sign(payload, process.env.MYSQL_KEY, { expiresIn: "7d" });
+
+    res.json({
+      token,
+    });
+  };
+  //중복확인  ok
+  DoubleCheck = async (req, res, next) => {
+    const { key, value } = req.body;
+    let user;
+    if (key == "userId") {
+      user = await this.userService.findOneUser_userId(value);
+    }
+    if (key == "nickname") {
+      user = await this.userService.findOneUser_nickname(value);
+    }
+    if (!user) {
+      return res.status(200).json({ ok: true });
+    }
+    return res.status(200).json({ ok: false });
+  };
+}
 module.exports = UsersController;
